7d02aba5607374f891b52d400a52b9df9263221a
nazar015 <github.job569@slmails.com>
2026-03-21 20:00:22 +0200

    Change namings, add common classes and GET  contacts endpoint

---
+++ ContactSystem.Application/Common/PagedResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactSystem.Application.Common
+{
+    public class PagedResult<TResult>
+    {
+        public IEnumerable<TResult> Items { get; set; }
+        public int Page { get; set; }
+        public int Size { get; set; }
+        public int Total { get; set; }
+    }
+}
--- ContactSystem/Controllers/ContactsController.cs
+++ ContactSystem/Controllers/ContactsController.cs
@@ -1,7 +1,6 @@
 ﻿using Asp.Versioning;
+using ContactSystem.Application.Common;
 using ContactSystem.Application.Dtos;
-using ContactSystem.Application.Entities;
-using ContactSystem.Application.Services;
 using ContactSystem.Application.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,40 +12,31 @@ namespace Granite.Controllers.Contacts
     [ApiVersion("1.0")]
     public class ContactsController : ControllerBase
     {
-        private readonly IContactsService _ContactsService;
-        private readonly IOfficesService _officeService;
+        private readonly IContactsService _contactsService;
 
-        public ContactsController(IContactsService ContactsService, OfficeService officeService)
+        public ContactsController(IContactsService contactsService)
         {
-            _ContactsService = ContactsService;
-            _officeService = officeService;
+            _contactsService = contactsService;
         }
 
         /// <summary>
         /// Retrieves a list of Contacts based on the provided search criteria.
         /// </summary>
-        /// <param name="name">The name to search for Contacts.</param>
-        /// <param name="page">The page number for pagination.</param>
-        /// <param name="pageSize">The number of records per page.</param>
         /// <returns>A list of Contacts that match the search criteria.</returns>
         /// <response code="200">Contacts retrieved successfully.</response>
         /// <response code="400">If the search term is empty or invalid.</response>
         /// <response code="500">If an internal server error occurs.</response>
-        [HttpGet("getContacts")]
-        [ProducesResponseType(typeof(ApiResponse<IEnumerable<ContactEntity>>), 200)]
-        [ProducesResponseType(typeof(ApiResponse<object>), 400)]
-        [ProducesResponseType(typeof(ApiResponse<object>), 500)]
+        [HttpGet]
+        [ProducesResponseType(typeof(PagedResult<ContactDto>), 200)]
+        [ProducesResponseType(typeof(PagedResult<ContactDto>), 400)]
+        [ProducesResponseType(typeof(PagedResult<ContactDto>), 500)]
         [Produces("application/json")]
         [MapToApiVersion("1.0")]
-        public async Task<IActionResult> GetContacts([FromQuery] string name, [FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+        public async Task<IActionResult> GetContacts([FromQuery] SearchQueryByOffice query)
         {
-            if (string.IsNullOrWhiteSpace(name))
-            {
-                return BadRequest(new ApiResponse<object>(false, "Search term cannot be empty.", null));
-            }
+            var dtos = await _contactsService.SearchAsync(query.OfficeId, query.SearchTerm, query.Page, query.PageSize);
 
-            //To Do
-            return Ok();
+            return Ok(dtos);
         }
     }
 }
